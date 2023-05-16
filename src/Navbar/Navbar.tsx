0e28751282cf54d2c786c3f05e039849e41ac8a9
Nidocq <phillip.linnemann.lundin@gmail.com>
2023-05-16 16:47:24 +0200

    Add details to navbar

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -22,23 +22,13 @@ export interface NavbarItems {
 function Navbar() {
     return ( 
         <div className='wrapper'>
-            <ul>
-                <li><img 
-                    className="logo" 
+                <img className="logo" 
                     src="https://cdn-icons-png.flaticon.com/512/6703/6703157.png" />
-                </li>
-                <li>
-                    <a href="./index.html">Explore</a>
-                </li>
-                <li>
-                    {/* TODO:  */}
-                    <a href="">Create</a>
-                </li>
-                <li><img 
+                <a href="./index.html">Explore</a>
+                <a href="">Create</a>
+                <img 
                     className="userlogo" 
                     src="https://cdn-icons-png.flaticon.com/512/1144/1144760.png" />
-                </li>
-            </ul>
         </div> );
 }
 
