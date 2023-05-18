c313e7e73c100ec95908ee252cacdf5ff0680282
Nidocq <phillip.linnemann.lundin@gmail.com>
2023-05-18 10:55:14 +0200

    Server, db running works with front end

--- nodeJSServer/abd_model.js
+++ nodeJSServer/abd_model.js
@@ -11,40 +11,41 @@ const pool = new Pool({
 
 const getItems = () => {
   return new Promise(function(resolve, reject) {
-    pool.query('SELECT * FROM Item ORDER BY id ASC', (error, results) => {
+    pool.query('SELECT * FROM item ORDER BY id ASC', (error, results) => {
       if (error) {
         reject(error)
       }
-      resolve(console.log("Hello i amn here", results));
+      // console.log(results)
+      resolve(results.rows);
     })
   }) 
 }
 
-const createItem = (body) => {
-  return new Promise(function(resolve, reject) {
-    const { name, email } = body
-    pool.query('INSERT INTO Item (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
-      if (error) {
-        reject(error)
-      }
-      resolve(`A new Item has been added added: ${results.rows[0]}`)
-    })
-  })
-}
-const deleteItem = () => {
-  return new Promise(function(resolve, reject) {
-    const id = parseInt(request.params.id)
-    pool.query('DELETE FROM Item WHERE id = $1', [id], (error, results) => {
-      if (error) {
-        reject(error)
-      }
-      resolve(`Item deleted with ID: ${id}`)
-    })
-      })
-}
+// const createItem = (body) => {
+//   return new Promise(function(resolve, reject) {
+//     const { name, email } = body
+//     pool.query('INSERT INTO Item (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+//       if (error) {
+//         reject(error)
+//       }
+//       resolve(`A new Item has been added added: ${results.rows[0]}`)
+//     })
+//   })
+// }
+// const deleteItem = () => {
+//   return new Promise(function(resolve, reject) {
+//     const id = parseInt(request.params.id)
+//     pool.query('DELETE FROM Item WHERE id = $1', [id], (error, results) => {
+//       if (error) {
+//         reject(error)
+//       }
+//       resolve(`Item deleted with ID: ${id}`)
+//     })
+//       })
+// }
 
 module.exports = {
   getItems,
-  createItem,
-  deleteItem,
+  // createItem,
+  // deleteItem,
 }
--- nodeJSServer/index.js
+++ nodeJSServer/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3001
 
 const Item_model = require('./abd_model')
-
+console.log(Item_model);
 app.use(express.json())
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -22,25 +22,27 @@ app.get('/', (req, res) => {
   })
 })
 
-app.post('/Items', (req, res) => {
-  Item_model.createItem(req.body)
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    res.status(500).send(error);
-  })
-})
+// fetch(API).then(do something with response)
 
-app.delete('/Items/:id', (req, res) => {
-  Item_model.deleteItem(req.params.id)
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    res.status(500).send(error);
-  })
-})
+// app.post('/Items', (req, res) => {
+//   Item_model.createItem(req.body)
+//   .then(response => {
+//     res.status(200).send(response);
+//   })
+//   .catch(error => {
+//     res.status(500).send(error);
+//   })
+// })
+
+// app.delete('/Items/:id', (req, res) => {
+//   Item_model.deleteItem(req.params.id)
+//   .then(response => {
+//     res.status(200).send(response);
+//   })
+//   .catch(error => {
+//     res.status(500).send(error);
+//   })
+// })
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
