776efaa3b3ac83ee447ac4e026be67c6cf12284f
Nidocq <phillip.linnemann.lundin@gmail.com>
2023-05-31 18:11:15 +0200

    init styling for userpage

--- ABD/src/UserPage/UserPage.tsx
+++ ABD/src/UserPage/UserPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useNavigate } from 'react-router';
 import Navbar from '../Navbar/Navbar';
+import './UserPage.css'
+import { Button } from '@chakra-ui/react';
 
 const UserPage = () => {
   return (
@@ -8,13 +10,34 @@ const UserPage = () => {
       <Navbar />
       <div className="bodyWrapper">
         <div className="ProfilePreview">
-          
-        </div>
-        <div className='ProfileEditor'>
+          <img src="https://cdn-icons-png.flaticon.com/512/848/848043.png"/>
+          <h2>Name Hanzomain</h2>
+          <p>Title maybe a specific seller</p>
+          <p>About page about this person, he is kind</p>
 
         </div>
+        <div className='ProfileEditor'>
+          <div className="nameUsername">
+            <h1>Profile</h1>
+            <input />
+            <h3>Full name</h3>
+            <h3>Username</h3>
+            <input />
+          </div>
+          <div className='prelim'>
+            <h3>Location</h3>
+            <input />
+            <h3>Bio</h3>
+            <input />
+          </div>
+          <Button 
+            className="btnSaveListing"
+            onClick={() => alert("Saved data")}
+            type="submit">
+            <b>Publish</b>
+          </Button>
+        </div>
       </div>
-      <p>UserPage</p>
     </>
   )
 }
