155fddcae7fcc8b3fa7bb4b4cf6f347364c89237
Nidocq <phillip.linnemann.lundin@gmail.com>
2023-06-04 23:02:16 +0200

    Add default state to secret

--- server/index.js
+++ server/index.js
@@ -21,10 +21,19 @@ app.use(
     credentials: true,
   })
 );
+
+
+// checking for null, ran into an issue if COOKIE_SECRET is not set
+// an error will be thrown. then default value will just be "secretValue"
+const secretEmpty = true;
+if (process.env.COOKIE_SECRET) { 
+  const secretEmpty = false;
+}
+
 app.use(express.json());
 app.use(
   session({
-    secret: process.env.COOKIE_SECRET,
+    secret: secretEmpty === false ? process.env.COOKIE_SECRET : "secretValue",
     credentials: true,
     name: "sid",
     resave: false,
