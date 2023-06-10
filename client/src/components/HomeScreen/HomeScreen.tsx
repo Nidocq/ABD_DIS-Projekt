7d05fc4f9d806c882adfba5b39d7c860e5c01f98
Nidocq <phillip.linnemann.lundin@gmail.com>
2023-06-10 23:37:19 +0200

    Homescreen styling with dark/light mode

--- client/src/components/HomeScreen/HomeScreen.tsx
+++ client/src/components/HomeScreen/HomeScreen.tsx
@@ -1,17 +1,54 @@
 import ListingItems from "../ListingItems/ListingItems";
 import "./HomeScreen.css";
 import Category from "../Categories/Category";
-import { useNavigate } from "react-router";
+import { ColorMode, useColorMode } from '@chakra-ui/react';
+import {
+  HStack, VStack,
+  Box,
+  Input,
+  InputRightAddon,
+  InputRightElement,
+  InputLeftElement,
+  InputGroup,
+  Image,
+  Img,
+  Text,
+  Heading,
+  Stack,
+  Button,
+  Card,
+  CardBody,
+  Textarea,
+  CardFooter,
+} from '@chakra-ui/react';
+import { colorTheme } from "../toggleColorMode";
 
 
 function HomeScreen() {
-  const navigate = useNavigate();
+  const { colorMode, toggleColorMode } = useColorMode(); 
+  var darklightmode = colorMode == colorTheme.dark ? "rgba(255,255,255,1)" : "rgba(0,0,0,1)"
   return (
     <main className="main-container">
       <div className="bodyClass">
         <div className="titleWrapper">
-          <h1>ABD</h1>
-          <h4>Get everything</h4>
+          <Heading
+            // linear gradient
+            // background={`linear-gradient(90deg, ${darklightmode} 59%, rgba(255,228,181,1) 78%)`} 
+            // raidal gradient
+            background={`radial-gradient(circle, ${darklightmode} 5%, rgba(255,228,181,1) 93%)`}
+            backgroundClip={"text"}
+            color={"transparent"} 
+            fontSize={"82px"}
+            >
+              ABD
+          </Heading>
+
+          <Heading
+            opacity={"0.6"}
+            fontSize={"16px"}
+            >
+              Get everything you search for!
+          </Heading>
         </div>
         <Category />
         <ListingItems />
